39181c2c3c56d6e60152f8ba7056cdb49b4f43a3
mateoALonso0& <alonsomateo2002@gmail.com>
2025-09-26 22:52:53 -0300

    Añade metodos adicionales a los repos

--- src/main/java/com/educatech/repository/IEnrollmentRepository.java
+++ src/main/java/com/educatech/repository/IEnrollmentRepository.java
@@ -1,9 +1,18 @@
 package com.educatech.repository;
 
+import com.educatech.entity.Course;
 import com.educatech.entity.Enrollment;
+import com.educatech.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IEnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    List<Enrollment> getEnrollmentsByCourse(Course course);
+
+    Enrollment getEnrollmentByStudentAndCourse(User student, Course course);
+
+    List<Enrollment> getEnrollmentsByStudent(User student);
 }
--- src/main/java/com/educatech/repository/IUserRepository.java
+++ src/main/java/com/educatech/repository/IUserRepository.java
@@ -1,9 +1,16 @@
 package com.educatech.repository;
 
 import com.educatech.entity.User;
+import com.educatech.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
+    User getUserByEmail(String email);
+
+    List<User> getUsersByRole(Role role);
 }
